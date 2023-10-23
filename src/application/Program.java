e20fcdf1c7e2090c929573817edbc3448846326a
Vitor <vitorsobrado7@gmail.com>
2023-10-23 15:54:23 -0300

    Find All

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,9 @@ public class Program {
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
 		
+		System.out.println("\n=== Teste 3: seller Find all ===");
+		list = sellerDao.findALl();
+		list.forEach(System.out::println);
+		
 	}
 }
